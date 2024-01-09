0ae5e5987ea3ab7cec5e6063e2a965bc6c92bca3
TalkingCoffeePot <barrberry666@gmail.com>
2024-01-09 17:10:33 +0500

    On branch main  Changes to be committed: 	modified:   accounts/forms.py     Добавлена валидация по почте и имени

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class NewUserForm(UserCreationForm):
@@ -6,3 +7,15 @@ class NewUserForm(UserCreationForm):
         model = User
         fields = ['username', 'password1', 'password2',
                   'first_name', 'last_name', 'email']
+        
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if len(email) < 1:
+            raise forms.ValidationError("Email обязателен к заполнению!")
+        return email
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        if len(cleaned_data['first_name']) < 1 and len(cleaned_data['last_name']) < 1:
+            raise forms.ValidationError("Хотя бы одно поле ИМЕНИ должно быть заполнено")
+        return cleaned_data
